83657cde43a85502b634897eb655a02a8ac227e3
abtin <abteen6@gmail.com>
2025-11-27 00:28:52 +0330

    Fix: Skip stored function on PostgreSQL

--- database/migrations/2019_11_21_194608_add_stored_function_to_get_url_path_of_category.php
+++ database/migrations/2019_11_21_194608_add_stored_function_to_get_url_path_of_category.php
@@ -7,49 +7,45 @@ class AddStoredFunctionToGetUrlPathOfCategory extends Migration
 {
     public function up()
     {
+        // فقط روی MySQL اجرا شود
         if (DB::getDriverName() !== 'mysql') {
             return;
         }
 
         $dbPrefix = DB::getTablePrefix();
-
-        $functionSQL = <<<SQL
-            DROP FUNCTION IF EXISTS get_url_path_of_category;
-            CREATE FUNCTION get_url_path_of_category(
-                categoryId INT,
-                localeCode VARCHAR(255)
-            )
-            RETURNS VARCHAR(255)
-            DETERMINISTIC
-            BEGIN
-                DECLARE urlPath VARCHAR(255);
-                IF categoryId <> 1 THEN
-                    SELECT
-                        GROUP_CONCAT(parent_translations.slug SEPARATOR '/') INTO urlPath
-                    FROM
-                        {$dbPrefix}categories AS node,
-                        {$dbPrefix}categories AS parent
-                        JOIN {$dbPrefix}category_translations AS parent_translations ON parent.id = parent_translations.category_id
-                    WHERE
-                        node._lft >= parent._lft
-                        AND node._rgt <= parent._rgt
-                        AND node.id = categoryId
-                        AND parent.id <> 1
-                        AND parent_translations.locale = localeCode
-                    GROUP BY
-                        node.id;
-
-                    IF urlPath IS NULL THEN
-                        SET urlPath = (SELECT slug FROM {$dbPrefix}category_translations WHERE category_id = categoryId);
+        $sql = "DROP FUNCTION IF EXISTS get_url_path_of_category;
+                CREATE FUNCTION get_url_path_of_category(
+                    categoryId INT,
+                    localeCode VARCHAR(255)
+                )
+                RETURNS VARCHAR(255)
+                DETERMINISTIC
+                BEGIN
+                    DECLARE urlPath VARCHAR(255);
+                    IF categoryId <> 1 THEN
+                        SELECT GROUP_CONCAT(parent_translations.slug SEPARATOR '/') INTO urlPath
+                        FROM {$dbPrefix}categories AS node,
+                             {$dbPrefix}categories AS parent
+                             JOIN {$dbPrefix}category_translations AS parent_translations 
+                               ON parent.id = parent_translations.category_id
+                        WHERE node._lft >= parent._lft
+                          AND node._rgt <= parent._rgt
+                          AND node.id = categoryId
+                          AND parent.id <> 1
+                          AND parent_translations.locale = localeCode
+                        GROUP BY node.id;
+                        
+                        IF urlPath IS NULL THEN
+                            SET urlPath = (SELECT slug FROM {$dbPrefix}category_translations 
+                                          WHERE category_id = categoryId);
+                        END IF;
+                    ELSE
+                        SET urlPath = '';
                     END IF;
-                ELSE
-                    SET urlPath = '';
-                END IF;
-                RETURN urlPath;
-            END
-SQL;
-
-        DB::unprepared($functionSQL);
+                    RETURN urlPath;
+                END";
+        
+        DB::unprepared($sql);
     }
 
     public function down()
@@ -57,6 +53,7 @@ SQL;
         if (DB::getDriverName() !== 'mysql') {
             return;
         }
+        
         DB::unprepared('DROP FUNCTION IF EXISTS get_url_path_of_category');
     }
 }
