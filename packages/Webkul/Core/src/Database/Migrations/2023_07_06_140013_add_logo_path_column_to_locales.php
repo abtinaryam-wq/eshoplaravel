c574e558ca27c4493cf44256cd48148bcc8c71e5
abtin <abteen6@gmail.com>
2025-11-27 01:53:18 +0330

    Fix CONCAT for PostgreSQL in locales migration

--- packages/Webkul/Core/src/Database/Migrations/2023_07_06_140013_add_logo_path_column_to_locales.php
+++ packages/Webkul/Core/src/Database/Migrations/2023_07_06_140013_add_logo_path_column_to_locales.php
@@ -2,38 +2,38 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
+class AddLogoPathColumnToLocales extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        if (Schema::hasColumn('locales', 'locale_image')) {
-            Schema::dropColumns('locales', 'locale_image');
+        if (! Schema::hasColumn('locales', 'logo_path')) {
+            Schema::table('locales', function (Blueprint $table) {
+                $table->string('logo_path')->nullable()->after('code');
+            });
         }
 
+        if (DB::getDriverName() === 'pgsql') {
+            DB::statement("
+                UPDATE locales
+                SET logo_path = 'locales/' || code || '.png'
+                WHERE logo_path IS NULL;
+            ");
+        } elseif (DB::getDriverName() === 'mysql') {
+            DB::statement("
+                UPDATE locales
+                SET logo_path = CONCAT('locales/', code, '.png')
+                WHERE logo_path IS NULL;
+            ");
+        }
+    }
+
+    public function down()
+    {
         Schema::table('locales', function (Blueprint $table) {
-            $table->string('logo_path')->after('direction')->nullable();
+            $table->dropColumn('logo_path');
         });
-
-        DB::table('locales')->whereNull('logo_path')->update(['logo_path'=> DB::raw('CONCAT("locales/", code, ".png")')]);
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        if (Schema::hasColumn('locales', 'locale_image')) {
-            Schema::dropColumns('locales', 'locale_image');
-        }
-
-        if (Schema::hasColumn('locales', 'logo_path')) {
-            Schema::dropColumns('locales', 'logo_path');
-        }
-    }
-};
+}
